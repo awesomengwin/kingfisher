671948a8f48be9599a5debc8be221fcc76d556ea
awesomengwin <awesomengwin@gmail.com>
2026-08-07 21:48:09 +0700

    Initial commit

---
+++ src/main/java/com/awesomengwin/kingfisher/KingfisherApplication.java
@@ -0,0 +1,13 @@
+package com.awesomengwin.kingfisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingfisherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KingfisherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/awesomengwin/kingfisher/KingfisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awesomengwin.kingfisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KingfisherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
