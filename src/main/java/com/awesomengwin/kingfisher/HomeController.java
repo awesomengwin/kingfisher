92fe7bf883e5cec0921a872de969e7d6cd22d936
awesomengwin <awesomengwin@gmail.com>
2026-08-09 00:01:34 +0700

    add common attribute currentUser

--- src/main/java/com/awesomengwin/kingfisher/HomeController.java
+++ src/main/java/com/awesomengwin/kingfisher/HomeController.java
@@ -1,10 +1,19 @@
 package com.awesomengwin.kingfisher;
 
+import com.awesomengwin.kingfisher.spotify.SpotifyUser;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class HomeController {
+
+    @ModelAttribute("currentUser")
+    public SpotifyUser currentUser(@AuthenticationPrincipal OAuth2User oauth2User) {
+        return new SpotifyUser(oauth2User);
+    }
 
     @GetMapping
     public String index() {
