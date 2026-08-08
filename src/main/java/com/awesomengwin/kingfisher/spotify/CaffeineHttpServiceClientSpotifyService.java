866c7767ca0e10e7279076393b740b74b69e90c9
awesomengwin <awesomengwin@gmail.com>
2026-08-08 23:08:48 +0700

    add class SpotifyService implementation, which depends on SpotifyApi and Caffeine

---
+++ src/main/java/com/awesomengwin/kingfisher/spotify/CaffeineHttpServiceClientSpotifyService.java
@@ -0,0 +1,41 @@
+package com.awesomengwin.kingfisher.spotify;
+
+import com.awesomengwin.kingfisher.spotify.valueobject.Page;
+import com.awesomengwin.kingfisher.spotify.valueobject.SavedTrack;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+@Service
+public class CaffeineHttpServiceClientSpotifyService implements SpotifyService {
+
+    private final SpotifyApi spotifyApi;
+    private final LoadingCache<String, Page<SavedTrack>> userSavedTracksLoadingCache;
+
+    public CaffeineHttpServiceClientSpotifyService(SpotifyApi spotifyApi) {
+        this.spotifyApi = spotifyApi;
+        this.userSavedTracksLoadingCache = Caffeine.newBuilder()
+                .maximumSize(500)
+                .expireAfterWrite(Duration.ofMinutes(10))
+                .build(this::loadUserSavedTracks);
+    }
+
+    @Override
+    public Page<SavedTrack> getUserSavedTracks(String userId, int limit, int offset) {
+        return userSavedTracksLoadingCache.get(generateSimpleKey(userId, limit, offset));
+    }
+
+    private Page<SavedTrack> loadUserSavedTracks(String key) {
+        String[] parts = key.split(":");
+        int limit = Integer.parseInt(parts[1]);
+        int offset = Integer.parseInt(parts[2]);
+
+        return spotifyApi.getUserSavedTracks(limit, offset);
+    }
+
+    private String generateSimpleKey(String userId, int limit, int offset) {
+        return userId + ":" + limit + ":" + offset;
+    }
+}
