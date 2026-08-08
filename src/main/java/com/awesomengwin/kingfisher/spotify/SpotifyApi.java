c2c220d2dee71bc7fad57476994e1418a32308d9
awesomengwin <awesomengwin@gmail.com>
2026-08-08 23:04:12 +0700

    fix invalid registrationId for user endpoint

--- src/main/java/com/awesomengwin/kingfisher/spotify/SpotifyApi.java
+++ src/main/java/com/awesomengwin/kingfisher/spotify/SpotifyApi.java
@@ -8,9 +8,9 @@ import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
 
 @HttpExchange
-@ClientRegistrationId("spotify-service")
 public interface SpotifyApi {
 
     @GetExchange("/me/tracks")
+    @ClientRegistrationId("spotify-login")
     Page<SavedTrack> getUserSavedTracks(@RequestParam int limit, @RequestParam int offset);
 }
