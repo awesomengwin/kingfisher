6638c8627644395a9f883652465e172ca6cab740
awesomengwin <awesomengwin@gmail.com>
2026-08-09 00:19:39 +0700

    add init method

--- src/main/java/com/awesomengwin/kingfisher/HomeController.java
+++ src/main/java/com/awesomengwin/kingfisher/HomeController.java
@@ -1,18 +1,38 @@
 package com.awesomengwin.kingfisher;
 
+import com.awesomengwin.kingfisher.spotify.SpotifyService;
 import com.awesomengwin.kingfisher.spotify.SpotifyUser;
+import com.awesomengwin.kingfisher.spotify.valueobject.Page;
+import com.awesomengwin.kingfisher.spotify.valueobject.SavedTrack;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
 
-    @ModelAttribute("currentUser")
-    public SpotifyUser currentUser(@AuthenticationPrincipal OAuth2User oauth2User) {
-        return new SpotifyUser(oauth2User);
+    private final SpotifyService spotifyService;
+
+    public HomeController(SpotifyService spotifyService) {
+        this.spotifyService = spotifyService;
+    }
+
+    @ModelAttribute
+    public void init(@AuthenticationPrincipal OAuth2User oauth2User,
+                     @RequestParam(defaultValue = "20") int limit,
+                     @RequestParam(defaultValue = "0") int offset,
+                     Model model) {
+        SpotifyUser spotifyUser = new SpotifyUser(oauth2User);
+
+        Page<SavedTrack> userSavedTracks =
+                spotifyService.getUserSavedTracks(spotifyUser.getName(), limit, offset);
+
+        model.addAttribute("currentUser", spotifyUser);
+        model.addAttribute("userSavedTracks", userSavedTracks);
     }
 
     @GetMapping
