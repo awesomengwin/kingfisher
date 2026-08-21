89f19d74d29afe0151ff5468b9a3ea55d0c3ddf3
awesomengwin <awesomengwin@gmail.com>
2026-08-21 21:22:37 +0700

    add LyricsClient

--- src/main/java/com/awesomengwin/kingfisher/config/HttpClientConfig.java
+++ src/main/java/com/awesomengwin/kingfisher/config/HttpClientConfig.java
@@ -1,5 +1,6 @@
 package com.awesomengwin.kingfisher.config;
 
+import com.awesomengwin.kingfisher.lyrics.client.LyricsClient;
 import com.awesomengwin.kingfisher.spotify.client.SpotifyClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +10,7 @@ import org.springframework.web.service.registry.ImportHttpServices;
 
 @Configuration
 @ImportHttpServices(group = "spotify", types = SpotifyClient.class)
+@ImportHttpServices(group = "lyrics", types = LyricsClient.class)
 public class HttpClientConfig {
 
     @Bean
