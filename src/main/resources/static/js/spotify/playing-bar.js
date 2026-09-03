302166105ee294b7cf46d4bf6cdd7dc07b4fd834
awesomengwin <awesomengwin@gmail.com>
2026-09-03 23:57:57 +0700

    set lyrics toggle visibility

--- src/main/resources/static/js/spotify/playing-bar.js
+++ src/main/resources/static/js/spotify/playing-bar.js
@@ -7,6 +7,7 @@ const ui = {
   trackName: playingBar.querySelector('[data-track-name]'),
   trackArtists: playingBar.querySelector('[data-track-artists]'),
   playerToggle: playingBar.querySelector('[data-player-toggle]'),
+  lyricsToggle: playingBar.querySelector('[data-lyrics-toggle]'),
   position: playingBar.querySelector('[data-position]'),
   duration: playingBar.querySelector('[data-duration]'),
   progress: playingBar.querySelector('[data-progress]'),
@@ -72,6 +73,7 @@ const handlePlayerStateChange = ({ detail: state }) => {
   ui.trackArtists.textContent = currentTrack.artists.map((artist) => artist.name).join(', ');
 
   ui.playerToggle.textContent = state.paused ? 'Play' : 'Pause';
+  ui.lyricsToggle.disabled = !currentTrack.id;
 
   ui.duration.textContent = getTimeFormatted(durationMs);
   ui.progress.max = durationMs;
