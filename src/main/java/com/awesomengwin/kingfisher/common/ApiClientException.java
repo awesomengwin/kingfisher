3e283396c910fae9b31f31cdc8141d4e68931524
awesomengwin <awesomengwin@gmail.com>
2026-09-06 01:55:40 +0700

    add Api client common error handling

---
+++ src/main/java/com/awesomengwin/kingfisher/common/ApiClientException.java
@@ -0,0 +1,22 @@
+package com.awesomengwin.kingfisher.common;
+
+import org.springframework.http.HttpStatusCode;
+
+public class ApiClientException extends RuntimeException {
+    private final String group;
+    private final HttpStatusCode statusCode;
+
+    public ApiClientException(String group, HttpStatusCode statusCode, String message) {
+        super(message);
+        this.group = group;
+        this.statusCode = statusCode;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public HttpStatusCode getStatusCode() {
+        return statusCode;
+    }
+}
