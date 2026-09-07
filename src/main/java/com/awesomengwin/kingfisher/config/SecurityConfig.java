64a6f3fb7ee6a70a3f0b7d1edeb13bc45734017b
awesomengwin <awesomengwin@gmail.com>
2026-09-08 01:28:09 +0700

    permit actuator/health

--- src/main/java/com/awesomengwin/kingfisher/config/SecurityConfig.java
+++ src/main/java/com/awesomengwin/kingfisher/config/SecurityConfig.java
@@ -3,9 +3,12 @@ package com.awesomengwin.kingfisher.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.client.JdbcOAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -15,5 +18,16 @@ public class SecurityConfig {
             JdbcOperations jdbcOperations,
             ClientRegistrationRepository clientRegistrationRepository) {
         return new JdbcOAuth2AuthorizedClientService(jdbcOperations, clientRegistrationRepository);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/actuator/health").permitAll()
+                        .anyRequest().authenticated())
+                .oauth2Login(Customizer.withDefaults());
+
+        return http.build();
     }
 }
