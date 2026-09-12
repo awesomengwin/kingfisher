d2bae3e2a63c6b3d506939d38b739ecfae3c48b3
awesomengwin <awesomengwin@gmail.com>
2026-09-12 21:53:13 +0700

    height, width nullable

--- src/main/java/com/awesomengwin/kingfisher/spotify/client/Image.java
+++ src/main/java/com/awesomengwin/kingfisher/spotify/client/Image.java
@@ -2,7 +2,7 @@ package com.awesomengwin.kingfisher.spotify.client;
 
 public record Image(
         String url,
-        int height,
-        int width
+        Integer height,
+        Integer width
 ) {
 }
