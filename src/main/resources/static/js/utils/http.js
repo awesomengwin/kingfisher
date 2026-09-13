28c4f26a48e105a874b5e62a31af0bcdfda1b761
awesomengwin <awesomengwin@gmail.com>
2026-09-13 11:28:13 +0700

    add put fn and throw Error if 4xx, 5xx

--- src/main/resources/static/js/utils/http.js
+++ src/main/resources/static/js/utils/http.js
@@ -1,11 +1,19 @@
 import { getCsrfHeader, getCsrfToken } from "./csrf.js";
 
 export const post = async (url, headers, body) => {
+  return doFetch(url, 'POST', headers, body);
+}
+
+export const put = async (url, headers, body) => {
+  return doFetch(url, 'PUT', headers, body);
+}
+
+const doFetch = async (url, method, headers, body) => {
   const csrfToken = getCsrfToken();
   const csrfHeader = getCsrfHeader();
 
   const options = {
-    method: 'POST',
+    method: method,
     headers: {
       'Content-Type': 'application/json',
       [csrfHeader]: csrfToken,
@@ -17,5 +25,11 @@ export const post = async (url, headers, body) => {
     options.body = JSON.stringify(body);
   }
 
-  return fetch(url, options);
+  const response = await fetch(url, options);
+
+  if (!response.ok) {
+    throw new Error(`HTTP ${response.status}`);
+  }
+
+  return response;
 }
