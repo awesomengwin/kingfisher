e90751da85e42e95bfd0ac6904e5ff1ad5055590
awesomengwin <awesomengwin@gmail.com>
2026-09-20 17:07:07 +0700

    add global exception

--- src/main/java/com/awesomengwin/kingfisher/common/ApiExceptionHandler.java
+++ src/main/java/com/awesomengwin/kingfisher/common/ApiExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.awesomengwin.kingfisher.common;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
@@ -35,6 +36,16 @@ public class ApiExceptionHandler {
 
         String title = "%s - %d %s".formatted(e.getGroup(), e.getStatusCode().value(), e.getStatusText());
         modelAndView.addObject("title", title);
+        modelAndView.addObject("message", e.getMessage());
+
+        return modelAndView;
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView handleGlobalException(Exception e) {
+        ModelAndView modelAndView = new ModelAndView("fragments/toast", HttpStatus.INTERNAL_SERVER_ERROR);
+
+        modelAndView.addObject("title", "Server - 500 Internal Server Error");
         modelAndView.addObject("message", e.getMessage());
 
         return modelAndView;
