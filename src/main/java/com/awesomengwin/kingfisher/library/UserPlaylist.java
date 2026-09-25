86011de940a4e02ddf4197c1651f011b283ee74b
awesomengwin <awesomengwin@gmail.com>
2026-09-25 22:04:48 +0700

    add functional type

---
+++ src/main/java/com/awesomengwin/kingfisher/library/UserPlaylist.java
@@ -0,0 +1,4 @@
+package com.awesomengwin.kingfisher.library;
+
+public record UserPlaylist(String name, String ownerName, Integer totalItems) {
+}
---
+++ src/main/java/com/awesomengwin/kingfisher/library/UserSavedAlbum.java
@@ -0,0 +1,6 @@
+package com.awesomengwin.kingfisher.library;
+
+import java.time.Instant;
+
+public record UserSavedAlbum(Instant addedAt, Album album) {
+}
