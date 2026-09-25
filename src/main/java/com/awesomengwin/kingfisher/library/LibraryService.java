7a458a7e752f7eda56ba0a670fb28700b4e68ac0
awesomengwin <awesomengwin@gmail.com>
2026-09-25 22:18:32 +0700

    add library service abstraction

---
+++ src/main/java/com/awesomengwin/kingfisher/library/LibraryService.java
@@ -0,0 +1,17 @@
+package com.awesomengwin.kingfisher.library;
+
+public interface LibraryService {
+
+    Page<UserSavedTrack> getUserSavedTracks(String userId, Pageable p);
+
+    Page<UserSavedAlbum> getUserSavedAlbums(String userId, Pageable p);
+
+    Page<UserPlaylist> getUserPlaylists(String userId, Pageable p);
+
+    Page<Track> getPlaylistTracks(String playlistId, String userId, Pageable p);
+
+    Page<Track> getAlbumTracks(String albumId, Pageable p);
+
+    Track getTrack(String trackId);
+
+}
