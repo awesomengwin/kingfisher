edf42bf9a7a5ac9f4ed9905021e4f4c46fa21222
awesomengwin <awesomengwin@gmail.com>
2026-09-26 16:44:40 +0700

    add SpotifyLibraryService and Pageable helper methods, mapper

---
+++ src/main/java/com/awesomengwin/kingfisher/spotify/SpotifyPlaylistTrackMapper.java
@@ -0,0 +1,9 @@
+package com.awesomengwin.kingfisher.spotify;
+
+import com.awesomengwin.kingfisher.library.Track;
+import org.mapstruct.Mapper;
+
+@Mapper(uses = SpotifyTrackMapper.class)
+public interface SpotifyPlaylistTrackMapper
+        extends SpotifyPageMapper<SpotifyPlaylistTrack, Track> {
+}
